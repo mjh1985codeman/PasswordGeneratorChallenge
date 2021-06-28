365af83e6852e29366cf5b69ec28d3bbf7af4b36
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-06-28 00:24:42 -0500

    added layout for code

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Assignment code here
 
+// step 1 - Prompt for user to input desired character length of password. 
+// step 2 - validate length for acceptacne criteria (min number of 8 characters, max number or characters 129)
+// step 3 - Prompt user to select desired character types (upper case letters, lower case letters, numbers -or- special characters).
+// step 4 - Validate user selction to make sure they selected at least ONE character type. 
+// step 5 - Generate Password according to user's input and validation results.  
+
+// variables. 
+
+function password() {
+  window.prompt(" Enter the Number of Characters you would like your password to be. ");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +26,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// function calls
+
+password();
