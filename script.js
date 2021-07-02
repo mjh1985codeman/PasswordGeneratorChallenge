a1debe107551e5751720c60cf422eb54c72f6fd0
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-02 00:22:10 -0500

    added userInput function for pw Length validation.

--- script.js
+++ script.js
@@ -8,9 +8,26 @@
 
 // variables. 
 
-function password() {
-  window.prompt(" Enter the Number of Characters you would like your password to be. ");
-}
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var special = ['!','@','#','$','%','^','&','*','+'];
+
+function userInput() {
+  var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
+    if (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN) {
+      alert("Your password must be between 8 and 128 characters!");
+      return;
+    }
+};
+
+
+
+
+// function calls
+userInput();
+
+// ****** Starter Code ****** //
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -26,8 +43,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// function calls
-
-password();
