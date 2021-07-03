f5c76c526862df1767ae69e70bc6467f7786af5b
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-02 21:52:37 -0500

    Created Functions to get user input

--- script.js
+++ script.js
@@ -8,34 +8,75 @@
 
 // variables. 
 
-var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var special = ['!','@','#','$','%','^','&','*','+'];
+const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+const special = ['!','@','#','$','%','^','&','*','+'];
 
-function userInput() {
-  var passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
+// Functions to get user input
+
+function userInputLength() {
+  const passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
     if (passwordLength < 8 || passwordLength > 128 || passwordLength === NaN) {
       alert("Your password must be between 8 and 128 characters!");
-      return;
     }
+  console.log(passwordLength);    
+};
+
+function lowerCaseConfirm() {
+  const lowerCaseChoice = (confirm("Would you like your password to contain Lowercase Letters?"));
+  console.log(lowerCaseChoice);
+  if (lowerCaseChoice === true) {
+    
+  }
+};
+
+function upperCaseConfirm() {
+  const upperCaseChoice = (confirm("Would you like your password to contain Uppercase Letters?"));
+  console.log(upperCaseChoice);
+  if (upperCaseChoice === true) {
+    
+  }
+};
+
+function numbersConfirm() {
+  const numbersChoice = (confirm("Would you like your password to contain Numbers?"));
+  console.log(numbersChoice);
+  if (numbersChoice === true) {
+  
+  }
 };
 
 
+function specialConfirm() {
+  const specialChoice = (confirm("Would you like your password to contain Special Characters?"));
+  console.log(specialChoice);
+  if (specialChoice === true) {
+  }
+};
 
 
 // function calls
-userInput();
+
+userInputLength();
+
+lowerCaseConfirm();
+
+upperCaseConfirm();
+
+numbersConfirm();
+
+specialConfirm();
 
 // ****** Starter Code ****** //
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
